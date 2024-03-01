d1548693a52753c0da67d1e0f753d326bd9c3cb4
taek00n <kimth0526@naver.com>
2024-03-01 23:05:28 +0900

    extends1

---
+++ src/extends1/ex/Item.java
@@ -0,0 +1,20 @@
+package extends1.ex;
+
+public class Item {
+
+    public String name;
+    public int price;
+
+    public Item(String name, int price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void print() {
+        System.out.println("이름 : " + name + ". 가격 : " + price);
+    }
+}
